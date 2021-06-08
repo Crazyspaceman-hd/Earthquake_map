74b71c3e191be538b87f5c9471bb19a53432273c
unknown <hvdeane@gmail.com>
2021-06-08 16:03:08 -0700

    submitting functional map

--- static/js/logic.js
+++ static/js/logic.js
@@ -20,37 +20,6 @@ function getColor(d) {
                       '#252525';
 }
 
-const outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/outdoors-v11",
-    accessToken: API_KEY
-});
-
-const darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "dark-v10",
-    accessToken: API_KEY
-});
-
-const satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 16,
-    id: "mapbox/satellite-v9",
-    accessToken: API_KEY
-});
-
-const myMap = L.map("map", {
-    center: [
-      0, 0
-    ],
-    zoom: 3,
-    layers: [satellite]
-  });
-
 function createFeatures(earthquakeData) {
 
   // Define a function we want to run once for each feature in the features array
@@ -79,23 +48,36 @@ function createFeatures(earthquakeData) {
       });
     }
   });
-  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then((data) =>{
-    const tectonic = L.geoJSON(data,{
-        style: {
-                opacity: 1,  
-                weight: 1.5
-        }
-    });  
-});
 
   // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes, mags, tectonic);
+  createMap(mags);
 }
 
-function createMap(earthquakes, mags) {
+function createMap(mags) {
 
   // Define streetmap and darkmap layers
+  const outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v11",
+    accessToken: API_KEY
+  });
 
+  const darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+
+  const satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 16,
+    id: "mapbox/satellite-v9",
+    accessToken: API_KEY
+});
 
   // Define a baseMaps object to hold our base layers
   const baseMaps = {
@@ -106,13 +88,17 @@ function createMap(earthquakes, mags) {
 
   // Create overlay object to hold our overlay layer
   const overlayMaps = {
-    Magnitudes: mags,
-    "Tectonic Plates": tectonic,
-    Earthquakes: earthquakes
+    Magnitudes: mags
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
-
+  const myMap = L.map("map", {
+    center: [
+      0, 0
+    ],
+    zoom: 3,
+    layers: [satellite, mags]
+  });
 
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
@@ -121,46 +107,13 @@ function createMap(earthquakes, mags) {
     collapsed: false
   }).addTo(myMap);
 
-
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then((data) =>{
+    L.geoJSON(data,{
+        style: {
+                opacity: 1,  
+                weight: 1.5
+        }
+    }).addTo(myMap);
+ 
+});
 }
-var legend = L.control({position: 'bottomright'});
-
-legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-        labels = [];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
-
-    return div;
-};
-
-legend.addTo(map);
-
-
-
-/* NOTE FOR STEP 2
-/  You can use the javascript Promise.all function to make two d3.json calls, 
-/  and your then function will not run until all data has been retreived.
-/
-/ ----------------------------------------
-/  Promise.all(
-/    [
-/        d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"),
-/        d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json")
-/    ]
-/  ).then( ([data,platedata]) => {
-/
-/        console.log("earthquakes", data)
-/        console.log("tectonic plates", platedata)
-/
-/    }).catch(e => console.warn(e));
-/
-/ ----------------------------------------*/
-
